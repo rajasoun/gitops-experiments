5b73110dcb1e48ec1a164e550ee5848b66faeab7
Raja Soundaramourty <rajasoun@icloud.com>
2022-12-29 00:33:57 +0530

    iterm script

---
+++ scripts/iterm2/watch.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/osascript
+
+tell application "iTerm"
+    activate
+    select first window
+    
+    # # Create new tab
+    # tell current window
+    #     create tab with default profile
+    # end tell
+    
+    # Split pane
+    tell current session of current window
+        split vertically with default profile
+        split horizontally with default profile
+    end tell
+    
+    # Exec commands
+    tell first session of current tab of current window
+        write text "watch flux get kustomizations"
+    end tell
+    tell second session of current tab of current window
+        write text "watch flux get all --all-namespaces"
+    end tell
+    tell third session of current tab of current window
+        write text "flux logs --all-namespaces --follow --tail=10"
+    end tell
+end tell
+
+
+
+
+
+
