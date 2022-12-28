36aead6bce73349d082fe5c1b31b659a30de5bb7
Raja Soundaramourty <rajasoun@icloud.com>
2022-12-29 01:17:59 +0530

    weave dashboard

--- scripts/iterm2/watch.sh
+++ scripts/iterm2/watch.sh
@@ -13,7 +13,7 @@ tell application "iTerm"
     tell current session of current window
         # split vertically with default profile
         split horizontally with default profile
-        split horizontally with default profile
+        # split horizontally with default profile
     end tell
     
     # Exec commands
@@ -21,11 +21,11 @@ tell application "iTerm"
         write text "watch flux get kustomizations"
     end tell
     tell second session of current tab of current window
-        write text "watch flux get all --all-namespaces"
-    end tell
-    tell third session of current tab of current window
         write text "flux logs --all-namespaces --follow --tail=10"
     end tell
+    # tell third session of current tab of current window
+    #     write text "watch flux get all --all-namespaces"
+    # end tell
 end tell
 
 
