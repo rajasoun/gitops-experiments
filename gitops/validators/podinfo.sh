8ca0c33dd63893579f19b310130d84dea887e3d8
Raja Soundaramourty <rajasoun@icloud.com>
2022-12-29 17:30:34 +0530

    move apps to a different branch

---
+++ gitops/validators/podinfo.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+IFS=$'\n\t'
+
+GIT_BASE_PATH=$(git rev-parse --show-toplevel)
+SCRIPT_LIB_DIR="$GIT_BASE_PATH/scripts/lib"
+
+function start_port_forward() {
+  kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 8080:80 > /dev/null 2>&1 &
+  sleep 1
+  server_pid=$!
+  echo $server_pid
+}
+
+function test(){
+  pretty_print "${BOLD}${UNDERLINE}Testing Podinfo${NC}\n"
+  pretty_print "${YELLOW}Starting Port Forward${NC}\n"
+  pid=$(start_port_forward)
+  pretty_print "${YELLOW}Test podinfo${NC}\n"
+  http http://dev.local.gd:8080 Host:podinfo.dev
+  echo -e "\n"
+  if [ $? -eq 0 ]; then
+    pass "podinfo test passed\n"
+  else
+    fail "podinfo test failed\n"
+  fi
+  kill $pid
+  line_separator
+}
+
+
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+source "${SCRIPT_LIB_DIR}/main.sh" $@
+wait 
+
+
