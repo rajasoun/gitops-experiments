f9ca48b5dc77f63aecc063779f507ae03374e0f0
Raja Soundaramourty <rajasoun@icloud.com>
2022-12-29 21:11:53 +0530

    kubernetes type configuration

--- local-dev/assist.sh
+++ local-dev/assist.sh
@@ -6,23 +6,37 @@ IFS=$'\n\t'
 GIT_BASE_PATH=$(git rev-parse --show-toplevel)
 SCRIPT_LIB_DIR="$GIT_BASE_PATH/scripts/lib"
 
+function get_kubernetes_type(){
+    load_env
+    if [ "$KUBERNETES_TYPE" == "k3d" ]; then
+        echo "k3d"
+    elif [ "$KUBERNETES_TYPE" == "kind" ]; then
+        echo "kind"
+    else
+        echo "k3d"
+    fi
+}
+
 function check(){
     [ ! -f .env ] && $GIT_BASE_PATH/local-dev/iaac/env/env.sh setup
     $GIT_BASE_PATH/local-dev/iaac/prerequisites/prerequisite.sh check 
     $GIT_BASE_PATH/local-dev/iaac/devops-tools/k9s/customize.sh check
     $GIT_BASE_PATH/local-dev/iaac/env/env.sh check
-    $GIT_BASE_PATH/local-dev/iaac/kubernetes/k3d/k3d.sh check
+    kubernetes_type=$(get_kubernetes_type)
+    $GIT_BASE_PATH/local-dev/iaac/kubernetes/$kubernetes_type/$kubernetes_type.sh check
 }
 
 function setup(){   
     $GIT_BASE_PATH/local-dev/iaac/prerequisites/prerequisite.sh setup 
     $GIT_BASE_PATH/local-dev/iaac/devops-tools/k9s/customize.sh setup
     $GIT_BASE_PATH/local-dev/iaac/env/env.sh setup
-    $GIT_BASE_PATH/local-dev/iaac/kubernetes/k3d/k3d.sh setup
+    kubernetes_type=$(get_kubernetes_type)
+    $GIT_BASE_PATH/local-dev/iaac/kubernetes/$kubernetes_type/$kubernetes_type.sh setup
 }
 
 function teardown(){
-    $GIT_BASE_PATH/local-dev/iaac/kubernetes/k3d/k3d.sh teardown
+    kubernetes_type=$(get_kubernetes_type)
+    $GIT_BASE_PATH/local-dev/iaac/kubernetes/$kubernetes_type/$kubernetes_type.sh teardown
     $GIT_BASE_PATH/local-dev/iaac/env/env.sh teardown
     $GIT_BASE_PATH/local-dev/iaac/devops-tools/k9s/customize.sh teardown
     #$GIT_BASE_PATH/local-dev/iaac/prerequisites/prerequisite.sh teardown
@@ -36,13 +50,15 @@ function test(){
     $GIT_BASE_PATH/local-dev/iaac/devops-tools/k9s/customize.sh test
     line_separator
     pretty_print "${BOLD}${UNDERLINE}k3d Tests\n${NC}"
-    $GIT_BASE_PATH/local-dev/iaac/kubernetes/k3d/k3d.sh test
+    kubernetes_type=$(get_kubernetes_type)
+    $GIT_BASE_PATH/local-dev/iaac/kubernetes/$kubernetes_type/$kubernetes_type.sh test
     line_separator
 }
 
 function status(){    
     $GIT_BASE_PATH/local-dev/iaac/prerequisites/prerequisite.sh status
-    $GIT_BASE_PATH/local-dev/iaac/kubernetes/k3d/k3d.sh status
+    kubernetes_type=$(get_kubernetes_type)
+    $GIT_BASE_PATH/local-dev/iaac/kubernetes/$kubernetes_type/$kubernetes_type.sh status
     pretty_print "${GREEN}${UNDERLINE}POD Status By Namespace\n${NC}" 
     pretty_print "${YELLOW}Executing -> kubectl get pods -A --sort-by='.metadata.namespace'\n${NC}" 
     kubectl get pods -A --sort-by='.metadata.namespace'
