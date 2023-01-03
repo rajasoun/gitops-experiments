9babb4f026ed6fc066a9c37af99d3fa3ac304d5e
Raja Soundaramourty <rajasoun@icloud.com>
2023-01-04 00:36:43 +0530

    helm based deployment for podinfo

--- scripts/lib/os.sh
+++ scripts/lib/os.sh
@@ -249,7 +249,7 @@ function kubeshark_hub(){
 
 # flux reconcile 
 function flux_reconcile(){
-    names=("infra-controllers" "infra-configs" "istio-system" "istio-gateway" "apps")
+    names=("infra-controllers" "infra-configs" "istio-system" "istio-gateway" "dashboard" "apps")
     for name in "${names[@]}"; do
         pretty_print "\n${BLUE}Reconciling $name${NC}\n"
         flux reconcile kustomization "$name" --with-source && pass "Reconciled $name\n" || fail "Failed to reconcile $name\n"
