2e0ffd2cc9f2cf783f6e8c06edff8d348023f086
Raja Soundaramourty <rajasoun@icloud.com>
2023-01-05 00:15:40 +0530

    refactor repository structure

--- gitops/assist.sh
+++ gitops/assist.sh
@@ -8,7 +8,6 @@ SCRIPT_LIB_DIR="$GIT_BASE_PATH/scripts/lib"
 
 #Run bootstrap for a public repository on a personal account
 function flux_bootstrap(){
-  flux install
   flux bootstrap github \
     --owner=$GITHUB_USER \
     --repository=$GITHUB_REPO \
