fb4b173af20f55b6557ffc889dfae6a6b99dad2e
Raja Soundaramourty <rajasoun@icloud.com>
2023-01-05 11:14:17 +0530

    fix github action failures

--- .github/actions/tools/entrypoint.sh
+++ .github/actions/tools/entrypoint.sh
@@ -1,29 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -eu
 
-YQ_VERSION="v4.30.5"
+YQ_VERSION="v4.30.6"
 KUSTOMIZE_VERSION="4.5.7"
 KUBECONFORM_VERSION="0.5.0"
 
-mkdir -p $GITHUB_WORKSPACE/bin
-cd $GITHUB_WORKSPACE/bin
+function create_workspace() {
+  if [ -z "${GITHUB_WORKSPACE:-}" ]; then
+    echo "GITHUB_WORKSPACE is not set"
+    exit 1
+  fi
 
-curl -sL https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -o yq
+  if [ ! -d "$GITHUB_WORKSPACE" ]; then
+    echo -e "GITHUB_WORKSPACE: $GITHUB_WORKSPACE"
+    mkdir -p $GITHUB_WORKSPACE/bin
+  fi
+}
 
-chmod +x $GITHUB_WORKSPACE/bin/yq
+function install_tools(){
+    create_workspace
+    cd $GITHUB_WORKSPACE/bin
+    curl -sL https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -o yq
+    chmod +x $GITHUB_WORKSPACE/bin/yq
 
-kustomize_url=https://github.com/kubernetes-sigs/kustomize/releases/download && \
-curl -sL ${kustomize_url}/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz | \
-tar xz
+    kustomize_url=https://github.com/kubernetes-sigs/kustomize/releases/download && \
+    curl -sL ${kustomize_url}/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz | tar xz
+    chmod +x $GITHUB_WORKSPACE/bin/kustomize
 
-chmod +x $GITHUB_WORKSPACE/bin/kustomize
+    curl -sL https://github.com/yannh/kubeconform/releases/download/v${KUBECONFORM_VERSION}/kubeconform-linux-amd64.tar.gz | tar xz
+    chmod +x $GITHUB_WORKSPACE/bin/kubeconform
+}
 
-curl -sL https://github.com/yannh/kubeconform/releases/download/v${KUBECONFORM_VERSION}/kubeconform-linux-amd64.tar.gz | \
-tar xz
-
-chmod +x $GITHUB_WORKSPACE/bin/kubeconform
-
+install_tools
 echo "$GITHUB_WORKSPACE/bin" >> $GITHUB_PATH
 echo "$RUNNER_WORKSPACE/$(basename $GITHUB_REPOSITORY)/bin" >> $GITHUB_PATH
 
