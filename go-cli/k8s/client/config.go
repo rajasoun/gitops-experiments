7f31db0eb70fa11e81115349b5b335f852e5a83e
Raja Soundaramourty <rajasoun@icloud.com>
2023-01-18 14:43:05 +0530

    k8s client

---
+++ go-cli/k8s/client/config.go
@@ -0,0 +1,34 @@
+package client
+
+import (
+	"flag"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func buildKubeConfig() (*rest.Config, error) {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"),
+			"(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	return config, err
+}
+
+func New() (*kubernetes.Clientset, error) {
+	config, err := buildKubeConfig()
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	return clientset, err
+}
