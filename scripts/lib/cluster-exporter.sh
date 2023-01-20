68274e9042326d581f7454239ff3f7d61d668744
Raja Soundaramourty <rajasoun@icloud.com>
2023-01-20 08:55:55 +0530

    cluster exporter

--- scripts/lib/cluster-exporter.sh
+++ scripts/lib/cluster-exporter.sh
@@ -144,18 +144,112 @@ function write_to_file() {
     filter_amazon_endpoints
 }
 
+# Function : Retrieve a list of all Kubernetes resources in the cluster.
+# Description:
+#  The kubectl api-resources command is used to get the list of all resources types available in the cluster and 
+#  the kubectl get command is used to get the resources. 
+#   --namespaced=false option is used to get the resources that are not namespaced 
+#   --verbs=list is used to get the list of resources. 
+#   -o=name option is used to format the output as "<resource_type>/<resource_name>" for easy readability. 
+function get_cluster_resources() {
+    # kubectl get -o=name pvc,configmap,serviceaccount,secret,ingress,service,deployment,statefulset,hpa,job,cronjob
+    # resource_types=(pvc configmap serviceaccount secret ingress service deployment statefulset hpa job cronjob)
+    # kubectl get "${resource_types[@]}" -o=name
+    resources=$(kubectl api-resources --namespaced=false --verbs=list -o=name)
+    kubectl get "${resources[@]}" -o=name
+}
+
+# Function : Export the YAML files from a cluster for a specific resource
+# Description:
+#   Exports the yaml representation of a specified Kubernetes resource to a file. 
+#   The function takes one argument, resource_name, which is the name of the resource to be exported. 
+#   Sets a local variable, resources_path, to a predefined path ($GIT_BASE_PATH/.resources), where the exported yaml files will be saved.
+#   Creates a subdirectory within resources_path that corresponds to the resource type (if it exists) using mkdir -p "$resources_path/$(dirname $resource_name)". 
+#   This allows for the exported yaml files to be organized by resource type, making it easier to find specific resources.
+#   Use kubectl command to retrieve the yaml representation of the specified resource using the kubectl get -o=yaml $resource_name command. 
+#   This command returns the yaml representation of the resource in the command line output. 
+#   The output is then redirected to a file located at "$resources_path/$resource_name.yaml" using > operator, creating a new file or overwrite the file if it already exists.
+# Parameters:
+#   resource_name: The name of the resource to be exported.
+function export_resource_yaml() {
+    local resources_path="$GIT_BASE_PATH/.resources"
+    local resource_name=$1
+    mkdir -p "$resources_path/$(dirname $resource_name)"
+    pretty_print "${YELLOW}Generating YAML files for resource=$resource${NC}\n"
+    kubectl get -o=yaml $resource_name > "$resources_path/$resource_name.yaml"
+}
+
+# Function : Export the YAML files from a cluster
+# Description:
+#   Export the YAML representation of resources in a cluster. 
+#   Gets a list of resources in the cluster using the function "get_cluster_resources", and then iterates through each resource in the list. 
+#   For each resource, it then calls the function "export_resource_yaml" and passes the resource as an argument, 
+#   which exports the YAML representation of the resource to a file.
+function export_yaml_from_cluster(){
+    local cluster_resources=$(get_cluster_resources)
+    for resource in $cluster_resources
+    do
+        export_resource_yaml $resource
+    done
+}
+
+
+# Function : Get helm releases from a cluster
+# Description:
+#   This function gets the list of Helm releases in the cluster using the helm list -q command.
+function get_helm_releases() {
+    helm list -q
+}
+
+# Function : Export helm Chart.yaml file from a cluster along with the values.yaml file and the reconstructed templates directory
+# Description:
+#   This function exports the helm Chart.yaml file from a cluster along with the values.yaml file and the reconstructed templates directory.
+#   The function takes one argument, release, which is the name of the release to be exported.
+#   Sets a local variable, resources_path, to a predefined path ($GIT_BASE_PATH/.resources), where the exported yaml files will be saved.
+#   Creates a subdirectory within resources_path that corresponds to the release name (if it exists) using mkdir -p "$resources_path/helm/$release".
+#   This allows for the exported yaml files to be organized by release name, making it easier to find specific releases.
+#   Use helm get all $release command to retrieve the yaml representation of the specified release.
+#   This command returns the yaml representation of the release in the command line output.
+#   The output is then redirected to a file located at "$resources_path/helm/$release/$release.yaml" using > operator, 
+#   creating a new file or overwrite the file if it already exists.
+function export_helm_chart_for_release() {
+    local resources_path="$GIT_BASE_PATH/.resources/helm"
+    local release=$1
+    mkdir -p "$resources_path/$release"
+    pretty_print "${YELLOW}Generating Helm charts for release=$release${NC}\n"
+    # download the entire helm chart for each release
+    helm get all $release > "$resources_path/$release/$release-all.yaml"
+    # download the values.yaml file
+    helm get values $release > "$resources_path/$release/$release-values.yaml"
+    # download the templates directory
+    helm get $release > "$resources_path/$release/$release-templates.tar.gz"
+    tar -xzf "$resources_path/$release/$release-templates.tar.gz" -C "$resources_path/$release"
+    rm "$resources_path/$release/$release-templates.tar.gz"
+}
+
+# Function : Export the Helm charts from a cluster
+# Description:
+#   This function exports the Helm charts from a cluster.
+#   Gets a list of Helm releases in the cluster using the function "get_helm_releases", and then iterates through each release in the list.
+#   For each release, it then calls the function "export_helm_chart_for_release" and passes the release as an argument,
+#   which exports the Helm chart for the release to a file.
+function export_helm_charts_from_cluster(){
+    local helm_releases=$(get_helm_releases)
+    for release in $helm_releases
+    do
+        export_helm_chart_for_release $release
+    done
+}
 
 # Function : usage
 # Description:
 #   Prints usage instructions and a list of valid options to the terminal.
 function usage() {
-    echo "${RED}Usage: $0 < stdout | file >${NC}"
-    cat <<-EOF
-Commands:
----------
-stdout        -> Prints the output to stdout  
-file          -> Prints the output to a file 
-EOF
+    error "Usage: $0 < env | helm | file >"
+    warn "\tAvailable options:"
+    info "\tenv:    Exports the cluster environment variables to a file named \"env_vars.csv\" in the current directory"
+    info "\thelm:   Exports the Helm charts to a directory named \".resources/helm\" in the current directory"
+    info "\tyaml:   Exports the YAML files to a directory named \".resources/yaml\" in the current directory"
 }
 
 # Function : Main function
@@ -173,9 +267,11 @@ function main(){
     opt="$1"
     choice=$( tr '[:upper:]' '[:lower:]' <<<"$opt" )
     case $choice in
-        stdout)print_tabular_output;;
-        file)write_to_file;;
-        *);;
+        env-list)print_tabular_output;;
+        env)write_to_file;;
+        helm)export_helm_charts_from_cluster;;
+        yaml)export_yaml_from_cluster;;
+        *) usage;;
     esac
 }
 
